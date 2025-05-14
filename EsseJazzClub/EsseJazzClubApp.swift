1adc135d4483e56bf71d8ab2629381cae4e088f5
Tigran Danielian <tigrandanielian@gmail.com>
2025-05-14 19:48:41 +0300

    Initial Commit

---
+++ EsseJazzClub/EsseJazzClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  EsseJazzClubApp.swift
+//  EsseJazzClub
+//
+//  Created by Tigran Danielian on 14.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct EsseJazzClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
